6b791c3f13126370ee3122a12315ce1bb9aeb2f1
Taufiq720 <taufiq.r360@gmail.com>
2026-07-29 01:10:48 +0600

    Product controller and Route added!

---
+++ src/controllers/productController.js
@@ -0,0 +1,92 @@
+import { ObjectId } from "mongodb";
+import { productsCollection } from "../config/database.js";
+
+export const getProducts = async (req, res) => {
+    try {
+        const products = await productsCollection.find().toArray();
+
+        res.send(products);
+    } catch (error) {
+        res.status(500).send({
+            success: false,
+            message: error.message,
+        });
+    }
+};
+
+export const getProductById = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const product = await productsCollection.findOne({
+            _id: new ObjectId(id),
+        });
+
+        res.send(product);
+    } catch (error) {
+        res.status(500).send({
+            success: false,
+            message: error.message,
+        });
+    }
+};
+
+export const createProduct = async (req, res) => {
+    try {
+        const product = req.body;
+
+        product.createdAt = new Date();
+
+        const result = await productsCollection.insertOne(product);
+
+        res.status(201).send({
+            success: true,
+            insertedId: result.insertedId,
+        });
+    } catch (error) {
+        res.status(500).send({
+            success: false,
+            message: error.message,
+        });
+    }
+};
+
+export const updateProduct = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const updatedData = req.body;
+
+        const result = await productsCollection.updateOne(
+            { _id: new ObjectId(id) },
+            {
+                $set: updatedData,
+            },
+        );
+
+        res.send(result);
+    } catch (error) {
+        res.status(500).send({
+            success: false,
+            message: error.message,
+        });
+    }
+};
+
+export const deleteProduct = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const result = await productsCollection.deleteOne({
+            _id: new ObjectId(id),
+        });
+
+        res.send(result);
+    } catch (error) {
+        res.status(500).send({
+            success: false,
+            message: error.message,
+        });
+    }
+};
+
